9567e266b3bb35c3f8d0f4e8e933d892c42868af
henrique.alexandre <developer.henrique1373@gmail.com>
2024-01-04 22:47:38 -0300

    refactor: send validations to other file, add call to handle and mapping errors

--- api/src/app/useCases/products/createProduct.ts
+++ api/src/app/useCases/products/createProduct.ts
@@ -1,76 +1,75 @@
+// Types
 import { Request, Response } from 'express';
-
+import { CreateProductItens } from '../../types/CreateProductItens';
+// Models
 import { Product } from '../../models/Product';
+import { Category } from '../../models/Category';
+// Validators
+import { createProductValidator } from '../../validators/createProductValidator';
+// Helpers
+import { handleError } from '../../helpers/handleError';
 
 export async function createProduct(req: Request, res: Response) {
   try {
-    const imagePath = req.file?.filename;
+    const itens: CreateProductItens = {
+      name: req.body?.name,
+      description: req.body?.description,
+      price: Number(req.body?.price),
+      category: req.body?.category,
+      ingredients: req.body?.ingredients,
+      industrialized: Boolean(req.body?.industrialized),
+      imagePath: req.file?.filename,
+    };
 
-    const { name, description, price, category, ingredients } = req.body;
+    const {
+      name,
+      description,
+      price,
+      category,
+      ingredients,
+      industrialized,
+      imagePath,
+    } = createProductValidator(itens);
 
-    if (!imagePath) {
-      return res
-        .status(400)
-        .json({ error: 'Required File', message: 'imagePath is required' });
-    }
+    const existCategory = await Category.findOne({ _id: category }).lean();
 
-    if (!name || !description || !price || !category || !ingredients) {
-      return res
-        .status(400)
-        .json({ error: 'Required Fields', message: 'All fields are required' });
+    if (!existCategory) {
+      throw {
+        type: 'NotFoundError',
+        message: 'category not found',
+      };
     }
 
     const existProduct = await Product.findOne({ name, category });
 
     if (existProduct) {
-      return res.status(409).json({
-        error: 'Duplicated Products',
+      throw {
+        type: 'DuplicatedResourceError',
         message: 'There is already a product with this name in this category',
-      });
-    }
-
-    const priceNumber = Number(price);
-    if (isNaN(priceNumber) || priceNumber <= 0) {
-      return res.status(400).json({
-        error: 'Price Error',
-        message: 'The price must be a number greater than zero',
-      });
-    }
-
-    let parsedIngredients;
-    try {
-      parsedIngredients = JSON.parse(ingredients);
-      if (!Array.isArray(parsedIngredients)) {
-        throw new Error();
-      }
-    } catch (error) {
-      return res.status(400).json({
-        error: 'Ingredients Format',
-        message: 'Ingredients must be provided in valid JSON array format',
-      });
+      };
     }
 
     const product = await Product.create({
       name,
       description,
       imagePath,
-      price: priceNumber,
+      price,
       category,
-      ingredients: parsedIngredients,
+      industrialized,
+      ingredients,
     });
 
     res.status(201).json(product);
-  } catch (error: unknown) {
+  } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({
         error: error.name,
         message: error.message,
       });
-    } else {
-      res.status(500).json({
-        error: 'Unknown error',
-        message: 'An unknown error has occurred',
-      });
     }
+
+    const { status, message, type } = handleError(error);
+
+    res.status(status).json({ error: type, message });
   }
 }
