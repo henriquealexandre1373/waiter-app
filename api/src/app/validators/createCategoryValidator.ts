793b22252b51e82203889e45d710d3a544d3b35c
henrique.alexandre <developer.henrique1373@gmail.com>
2024-02-16 11:31:48 -0300

    refactor: change validations from createCategory to createCategoryValidator and add descriptions

---
+++ api/src/app/validators/createCategoryValidator.ts
@@ -0,0 +1,41 @@
+import { Category } from '../models/Category'
+
+/**
+ * Validates the creation of a category.
+ *
+ * This function checks whether the provided icon and name meet the required criteria for category creation.
+ * It also ensures that a category with the given name doesn't already exist in the database.
+ *
+ * @param {string} icon - The icon for the category.
+ * @param {string} name - The name of the category.
+ *
+ * @throws {object} Throws an exception if validation fails, including:
+ *   - type: 'RequiredResourceError' if icon or name is missing.
+ *   - type: 'DuplicatedResourceError' if a category with the provided name already exists.
+ *
+ * @returns {Promise<void>} Resolves if validation is successful; otherwise, an exception is thrown.
+ */
+export async function createCategoryValidator(
+  icon: string,
+  name: string
+): Promise<void> {
+  // Check if icon and name are provided
+  if (!icon || !name) {
+    throw {
+      type: 'RequiredResourceError',
+      message: 'Properties icon and name are required',
+    }
+  }
+
+  // Check if a category with the given name already exists
+  const existCategory = await Category.findOne({ name })
+
+  if (existCategory) {
+    throw {
+      type: 'DuplicatedResourceError',
+      message: 'A category with this name already exists',
+    }
+  }
+
+  return
+}
