5a583f910e66414b327148578316916828606d26
henriquealexandre1373 <developer.henrique1373@gmail.com>
2024-09-29 10:51:02 -0300

    chore: configure babel, linters and jest

---
+++ api/jest.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { resolve } = require('path');
+const root = resolve(__dirname);
+
+module.exports = {
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  clearMocks: true,
+  rootDir: root,
+  verbose: true,
+  displayName: 'root-tests',
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '@src/(.*)$': '<rootDir>/src/$1',
+    '@test/(.*)$': '<rootDir>/test/$1',
+    '@useCases/(.*)$': '<rootDir>/src/app/useCases/$1',
+    '@interfaces/(.*)$': '<rootDir>/src/app/interfaces/$1',
+    '@validators/(.*)$': '<rootDir>/src/app/validators/$1',
+    '@customTypes/(.*)$': '<rootDir>/src/app/customTypes/$1',
+    '@models/(.*)$': '<rootDir>/src/app/models/$1',
+    '@services/(.*)$': '<rootDir>/src/app/services/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: [resolve(__dirname, 'src'), resolve(__dirname, 'test')],
+  projects: [
+    {
+      displayName: 'unit',
+      testMatch: ['<rootDir>/test/unit/**/*.test.ts'],
+      coverageDirectory: 'coverage/unit',
+    },
+    {
+      displayName: 'integration',
+      testMatch: ['<rootDir>/test/integration/**/*.test.ts'],
+      maxWorkers: 1,
+      detectOpenHandles: true,
+    },
+    {
+      displayName: 'e2e',
+      testMatch: ['<rootDir>/test/e2e/**/*.test.ts'],
+      maxWorkers: 1,
+      detectOpenHandles: true,
+      globalSetup: '<rootDir>/test/e2e/global-setup.js',
+      globalTeardown: '<rootDir>/test/e2e/global-teardown.js',
+      setupFilesAfterEnv: ['<rootDir>/test/e2e/jest-setup.ts'],
+    },
+  ],
+};
