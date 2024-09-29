e7e4b245dfd52839a4afdb7615a93c122b88dad8
henriquealexandre1373 <developer.henrique1373@gmail.com>
2024-09-29 10:00:56 -0300

    refactor: create middleware errorHandler and delete old handleError helper

---
+++ api/src/app/interfaces/http/middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,38 @@
+import logger from '../../../services/loggerService'
+import {Request, Response, NextFunction} from 'express';
+
+
+
+export default function errorHandler (err: any, req: Request, res: Response, next: NextFunction) {
+  logger.error(`${err.type || 'Unknown'} - ${err.message}`)
+  if (err.stack ) logger.error(err.stack)
+
+  const baseError = {
+    error: (err as { error?: string }).error,
+    message: (err as { message?: string }).message,
+  }
+
+  let statusCode = 500
+
+  switch (err.type) {
+    case 'RequiredResourceError':
+      statusCode = 400
+      break;
+    case 'TypeError':
+      statusCode = 400
+      break;
+    case 'DuplicatedResourceError':
+      statusCode = 409
+      break;
+    case 'NotFoundError':
+      statusCode = 404
+      break;
+    default:
+      baseError.error = 'Unknown error'
+      baseError.message = 'An unknown error has occurred'
+  }
+
+  res.status(statusCode).json({
+    ...baseError
+  })
+}
--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 import { router } from './router';
+import errorHandler from './app/interfaces/http/middlewares/errorHandlerMiddleware';
 
 mongoose
   .connect('mongodb://localhost:27017')
@@ -13,6 +14,7 @@ mongoose
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
+    app.use(errorHandler)
 
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
