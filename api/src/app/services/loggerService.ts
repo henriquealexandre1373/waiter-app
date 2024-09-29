25ee39e302c8b4ca5feb5d41fad730bbc2182dac
henriquealexandre1373 <developer.henrique1373@gmail.com>
2024-09-29 10:01:34 -0300

    feat: add winston to generate logs for debug

---
+++ api/src/app/services/loggerService.ts
@@ -0,0 +1,15 @@
+import winston from 'winston'
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({format: 'DD-MM-YYYY HH:mm:ss'}),
+    winston.format.printf(({level, message, timestamp}) => {
+      return `[${timestamp}] - ${level}: ${message}`
+    })
+  ),
+  transports: [new winston.transports.Console()]
+})
+
+export default logger
