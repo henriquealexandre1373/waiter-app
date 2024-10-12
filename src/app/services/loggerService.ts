c3d5d7f68c3abb70ca3e53bec4cf068f9a4fd69a
henriquealexandre1373 <developer.henrique1373@gmail.com>
2024-10-12 11:02:10 -0300

    chore: adjust logger to not execute in test environments

--- src/app/services/loggerService.ts
+++ src/app/services/loggerService.ts
@@ -1,7 +1,7 @@
 import winston from 'winston';
 
 const logger = winston.createLogger({
-  level: 'info',
+  level: process.env.NODE_ENV === 'test' ? 'silent' : 'info',
   format: winston.format.combine(
     winston.format.colorize(),
     winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
